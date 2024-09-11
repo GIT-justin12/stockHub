ef3d4e54aa946e68229b644671e8b2345702289c
GIT-justin12 <agbozojustin42@gmail.com>
2024-09-11 12:56:36 +0000

    Add of header on home page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/login'
+import Home from './pages/home'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />}></Route>
-          <Route></Route>
+          <Route path="/home" element={<Home />}></Route>
           <Route></Route>
         </Routes>
       </Router>
---
+++ front/src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from '../assets/logo.png'
+import profile from '../assets/user.svg'
+
+const Header = () => {
+    return (
+        <header className='flex items-center px-6 py-2 drop-shadow-md shadow-[#d5d5d5] border-b-2'>
+            <div className='mr-auto'><img className='h-8' src={logo} alt="Logo de stockHub" /></div>
+            <div className='flex items-end justify-center'><span className='py-2'>Justin </span> <img src={profile} alt="Icon de profile" /></div>
+        </header>
+    )
+}
+
+export default Header
